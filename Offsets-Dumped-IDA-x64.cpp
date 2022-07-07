bb483b6c8d690fa23e5a78afa2d8717666c84535
MrGameLeaks <108452509+MrGameLeaks@users.noreply.github.com>
2022-07-06 21:08:30 -0700

    Add files via upload

---
+++ Offsets-Dumped-IDA-x64.cpp
@@ -0,0 +1,37 @@
+DecryptFNameWide: 0x7751104
+FreeMemory: 0xD8A8F0
+DecryptFNameAnsi: 0xD4F2C0
+GObjects or GUObjectArray: 0xCC94048
+GWorld or UWorld: 0xCE461F8
+ProcessEvent: 0xF27394
+GNames or fNamePool: 0xCE99D40
+
+//I believe these are updated idk i had to go on a holiday.
+PlayerState: 0x290
+AActors: 0x98
+ActorCount: 0xA0
+RootComponent: 0x188
+Mesh: 0x2f0
+OwningGameInstance: 0x1A8
+DisplayName: 0x90
+Tier: 0x74
+LastFireTimeVerified: 0xa74
+RelativeRotation: 0x140
+TeamIndex: 0x1070
+LocalPlayers: 0x38
+LastFireAbilityTime: 0xd60
+CurrentWeapon: 0x790
+bADSWhileNotOnGround: 0x4805
+WeaponData: 0x3d8
+LastFireTime: 0xa70
+Levels: 0x160
+ItemDefinition: 0x18
+PersistentLevel: 0x30
+FireStartLoc: 0x9f0
+RelativeLocation: 0x128
+PlayerController: 0x30
+PlayerCameraManager: 0x328
+AcknowledgedPawn: 0x310
+PrimaryPickupItemEntry: 0x2f8
+
+//Sigs coming soon when im back home
